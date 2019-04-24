7e689806ead32fde0302d2023a175c1ffba0704d
hiroki hokari <hiro.hokari.0722@gmail.com>
2019-04-24 19:03:59 +0700

    Add Contact page template

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,16 +2,65 @@ import React from 'react';
 
 // material-ui
 import { withStyles } from '@material-ui/core/styles';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import Typography from '@material-ui/core/Typography';
+
+// material-ui icons
+import Phone from '@material-ui/icons/Phone';
+import Email from '@material-ui/icons/Email';
+import People from '@material-ui/icons/People';
+import HeadsetMic from '@material-ui/icons/HeadsetMic';
 
 const styles = theme => ({
-  root: {},
+  root: {
+    display: 'flex',
+    justifyContent: 'center',
+  },
 });
 
 const Contact = (props) => {
   const { classes } = props;
 
   return (
-    <div>Contact Section</div>
+    <div className={classes.root}>
+      <List>
+        <ListItem className={classes.item}>
+          <ListItemIcon>
+            <Phone/>
+          </ListItemIcon>
+          <ListItemText
+            primary="+66909235430">
+          </ListItemText>
+        </ListItem>
+        <ListItem className={classes.item}>
+          <ListItemIcon>
+            <Email/>
+          </ListItemIcon>
+          <ListItemText
+            primary="hiro.hokari.0722@gmail.com">
+          </ListItemText>
+        </ListItem>
+        <ListItem className={classes.item}>
+          <ListItemIcon>
+            <People/>
+          </ListItemIcon>
+          <ListItemText
+            primary="linkedin.com/in/hiroki-h">
+          </ListItemText>
+        </ListItem>
+        <ListItem className={classes.item}>
+          <ListItemIcon>
+            <HeadsetMic/>
+          </ListItemIcon>
+          <ListItemText
+            primary="Zoom/Skype etc upon request">
+          </ListItemText>
+        </ListItem>
+      </List>
+    </div>
   );
 }
 
