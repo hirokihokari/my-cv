908989e5d1053b0809ec02a5e3064f718979c836
hiroki hokari <hiro.hokari.0722@gmail.com>
2019-05-10 17:19:46 +0700

    Add transition to BackToTop button

--- src/components/BackToTop.jsx
+++ src/components/BackToTop.jsx
@@ -7,27 +7,54 @@ import Fab from '@material-ui/core/Fab';
 // material-ui icons
 import ArrowUpward from '@material-ui/icons/ArrowUpward';
 
+// own
+import { withFullPageContext } from './FullPageProvider';
+
 const styles = theme => ({
   root: {
     position: 'fixed',
     right: '1rem',
-    bottom: '1rem',
+    bottom: '-5rem',
+    transition: 'bottom 0.5s 0.3s',
+    '&.active': {
+      bottom: '1rem',
+    },
+  },
+  icon: {
+    position: 'absolute',
+    bottom: '0',
+    left: '50%',
+    transform: 'translate(-50%, 50%)',
+    transition: 'bottom 0.5s 0.3s',
+    '&.active': {
+      bottom: '50%',
+    },
   },
 });
 
 const BackToTop = (props) => {
-  const { classes } = props;
+  const { fullPageContext, classes } = props;
+
+  const active = fullPageContext.state.currentIndex != 0;
+
+  const rootClasses = classes.root + (active ? " active" : "");
+  const iconClasses = classes.icon + (active ? " active" : "");
 
   return (
     <Fab
       href="#Title"
       color="primary"
       aria-label="Back"
-      className={classes.root}
+      className={rootClasses}
+      variant="extended"
       id="backToTop">
-      <ArrowUpward />
+      <ArrowUpward className={iconClasses}/>
     </Fab>
   );
 }
 
-export default withStyles(styles)(BackToTop);
+export default withStyles(styles)(
+  withFullPageContext(
+    BackToTop
+  )
+);
