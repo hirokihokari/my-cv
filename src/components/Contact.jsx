ad554e4c6f28984b09aa4c2f81af9c77d5f3f04d
hiroki hokari <hiro.hokari.0722@gmail.com>
2020-05-21 16:02:07 +0900

    Add description in Project page

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -6,7 +6,6 @@ import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
-import Typography from '@material-ui/core/Typography';
 
 // material-ui icons
 import Phone from '@material-ui/icons/Phone';
